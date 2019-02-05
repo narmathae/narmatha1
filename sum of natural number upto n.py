c4cf5c8277ac5c04226ec15e25b0f49d848bf24f
narmathae <47316289+narmathae@users.noreply.github.com>
2019-02-05 15:20:52 +0530

    Create sum of natural number upto n.py

---
+++ sum of natural number upto n.py
@@ -0,0 +1,17 @@
+
+   #python program to find the sum of natural numbers up to n where n is provided by user
+   #change this value for a different result
+   num=16
+   #uncomment to take input from the user
+   #num = int(input("Enter a number:"))
+   if num < 0:
+   print("Enter a positive number:"))
+   if num < 0;
+   print("Enter a positive number")
+   else:
+   sum=0
+   #use  while loop to iterate un till zero
+   while (num>0):
+   sum+= num
+   num-=1
+   print("the sum is",sum)
