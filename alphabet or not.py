87988098a790d5891b50b482b78301707bfb7b3a
narmathae <47316289+narmathae@users.noreply.github.com>
2019-02-05 12:44:59 +0530

    Create alphabet or not.py

---
+++ alphabet or not.py
@@ -0,0 +1,7 @@
+
+    #taking user input
+    ch= input("Enter a character:")
+    if((ch>'a' and ch<= 'z') or (ch>='A' and ch<='Z')):
+    print(ch, "is an Alphabet")
+    else:
+    print (ch,"is not an Alphabet")
